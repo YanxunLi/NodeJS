be7e912a836d2982c07035b711be0acd13d57217
liyanxun <liyanxun1128@gmail.com>
2020-04-04 23:31:59 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: 'Your Client App ID',
+        clientSecret: '05b04bf1c466210553d8d78af7881c75'
+    }
 }
