e99b3227b9d9b64d64a11cf5682bb7402c39b069
liyanxun <liyanxun1128@gmail.com>
2020-04-04 20:36:52 -0700

    Cors

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 var User = require('../models/user');
 var passport = require('passport');
 var authenticate = require('../authenticate');
+const cors = require('./cors');
 
 var router = express.Router();
 router.use(bodyParser.json());
@@ -11,8 +12,7 @@ router.use(bodyParser.json());
 /*router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });*/
-router.route('/')
-.get(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
+router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
   User.find({})
   .then((users) => {
     res.statusCode = 200;
@@ -22,7 +22,7 @@ router.route('/')
   .catch((err) => next(err));
 });
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', cors.corsWithOptions, (req, res, next) => {
   User.register(new User({username: req.body.username}),
     req.body.password, (err, user) => {
     if(err) {
@@ -52,7 +52,7 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
 
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
